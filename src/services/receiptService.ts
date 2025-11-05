000267fa72c2c85e4b4f1679498122461a5ed4df
Mario Vojnic <mariovojnichajduk@gmail.com>
2025-11-05 22:57:16 +0100

    collecting only once

--- src/services/receiptService.ts
+++ src/services/receiptService.ts
@@ -13,6 +13,8 @@ export interface ProcessReceiptResponse {
   storeName?: string;
   rawStoreName?: string;
   shopId?: string;
+  receiptId?: string;
+  shopLocation?: string;
 }
 
 class ReceiptService {
